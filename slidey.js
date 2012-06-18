4f4f1e9b48fa682c1bf976fdc20f6d5d058d6303
Darryl Pinto <dazpinto@gmail.com>
2012-06-18 22:13:05 +0100

    option to put prev/next buttons either side of carousel

--- slidey.js
+++ slidey.js
@@ -6,7 +6,8 @@
       var settings = $.extend( {
         'speed'  : 500,
         'element_width'  : children.first().width(),
-        'element_height' : children.first().height()
+        'element_height' : children.first().height(),
+        'controls_either_side' : false
       }, options);
       var full_width = children.length * settings.element_width;
 
@@ -18,28 +19,34 @@
       .end()
       .css('overflow','hidden')
       .width(settings.element_width)
-      .height(settings.element_height)
-      .parent()
-      .append(
-        $('<a href="#">Previous</a>').click(function(e){
+      .height(settings.element_height);
+
+      var prev_button = $('<a href="#">Previous</a>').click(function(e){
           e.preventDefault();
           if ($(target).scrollLeft() == 0){
             $(target).animate({scrollLeft:full_width},settings.speed)
           }else{
             $(target).animate({scrollLeft:"-="+settings.element_width},settings.speed)
           }
-        })
-      )
-      .append(
-        $('<a href="#">Next</a>').click(function(e){
+        });
+
+      var next_button = $('<a href="#">Next</a>').click(function(e){
           e.preventDefault();
           if ($(target).scrollLeft() == (full_width - settings.element_width)){
             $(target).animate({scrollLeft: 0},settings.speed)
           }else{
             $(target).animate({scrollLeft:"+="+settings.element_width},settings.speed)
           }
-        })
-      );
+        });
+
+      if (settings.controls_either_side){
+        target.parent()
+          .prepend(prev_button).append(next_button);
+      }
+      else{
+        target.parent()
+          .append(prev_button).append(next_button);
+      }
     });
   };
 })( jQuery );
