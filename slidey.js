5a72022b74863a7bf7d21bfba4af53121db24897
Darryl Pinto <dazpinto@gmail.com>
2012-06-18 21:59:38 +0100

    add options and allow chaining

--- slidey.js
+++ slidey.js
@@ -1,39 +1,45 @@
 (function( $ ) {
-  $.fn.slidey = function() {
-  	var full_width = 0;
-    var element_height = this.children().first().height();
-    var element_width = this.children().first().width();
-    var full_width = this.children().length * element_width;
-    var target = this;
-    this
-      .children()
-        .css('float','left')
-        .width(element_width)
-        .wrapAll($('<div id="aaa" />').width(full_width))
-    .end()
-    .css('overflow','hidden')
-    .width(element_width)
-    .height(element_height)
-    .parent()
-    .append(
-      $('<a href="#">Previous</a>').click(function(e){
-        e.preventDefault();
-        if ($(target).scrollLeft() == 0){
-          $(target).animate({scrollLeft:full_width},500)
-        }else{
-          $(target).animate({scrollLeft:"-="+element_width},500)
-        }
-      })
-    )
-    .append(
-      $('<a href="#">Next</a>').click(function(e){
-        e.preventDefault();
-        if ($(target).scrollLeft() == (full_width - element_width)){
-          $(target).animate({scrollLeft: 0},500)
-        }else{
-          $(target).animate({scrollLeft:"+="+element_width},500)
-        }
-      })
-    );
+  $.fn.slidey = function(options) {
+    return this.each(function() {
+      var target = $(this);
+      var children = target.children();
+      var settings = $.extend( {
+        'speed'  : 500,
+        'element_width'  : children.first().width(),
+        'element_height' : children.first().height()
+      }, options);
+      var full_width = children.length * settings.element_width;
+
+      target
+        .children()
+          .css('float','left')
+          .width(settings.element_width)
+          .wrapAll($('<div />').width(full_width))
+      .end()
+      .css('overflow','hidden')
+      .width(settings.element_width)
+      .height(settings.element_height)
+      .parent()
+      .append(
+        $('<a href="#">Previous</a>').click(function(e){
+          e.preventDefault();
+          if ($(target).scrollLeft() == 0){
+            $(target).animate({scrollLeft:full_width},settings.speed)
+          }else{
+            $(target).animate({scrollLeft:"-="+settings.element_width},settings.speed)
+          }
+        })
+      )
+      .append(
+        $('<a href="#">Next</a>').click(function(e){
+          e.preventDefault();
+          if ($(target).scrollLeft() == (full_width - settings.element_width)){
+            $(target).animate({scrollLeft: 0},settings.speed)
+          }else{
+            $(target).animate({scrollLeft:"+="+settings.element_width},settings.speed)
+          }
+        })
+      );
+    });
   };
 })( jQuery );
